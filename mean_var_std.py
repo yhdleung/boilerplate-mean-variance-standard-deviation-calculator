a9d1300e2e254f8bf58684bd4b381d7b15bfa9b7
Dick Leung <yhd.leung@gmail.com>
2021-08-08 22:50:39 +0800

    do assignment, implement calculate

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,24 @@ import numpy as np
 
 def calculate(list):
 
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
 
+    np_matrix = np.array(list).reshape(3, 3)
+    # required the result to be lists and not Numpy arrays.
+    means = [ np_matrix.mean(axis=0).tolist() , np_matrix.mean(axis=1).tolist(), np_matrix.mean() ]
+    vars = [ np_matrix.var(axis=0).tolist(), np_matrix.var(axis=1).tolist(), np_matrix.var() ]
+    stds = [ np_matrix.std(axis=0).tolist(), np_matrix.std(axis=1).tolist(), np_matrix.std() ]
+    maxs = [ np_matrix.max(axis=0).tolist(), np_matrix.max(axis=1).tolist(), np_matrix.max() ]
+    mins = [ np_matrix.min(axis=0).tolist(), np_matrix.min(axis=1).tolist(), np_matrix.min() ]
+    sums = [ np_matrix.sum(axis=0).tolist(), np_matrix.sum(axis=1).tolist(), np_matrix.sum() ]
 
-
+    calculations = {
+        'mean': means,
+        'variance': vars,
+        'standard deviation': stds,
+        'max': maxs,
+        'min': mins,
+        'sum': sums
+    }
     return calculations
